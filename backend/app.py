314d6f8b66794b55d2fee12c8524832739bc406f
Mbee0 <mbee@cpp.edu>
2026-02-25 23:04:18 -0800

    trying new app.run message in main

--- backend/app.py
+++ backend/app.py
@@ -171,4 +171,4 @@ def toggle_star(recipe_id):
     return jsonify(recipe_doc_to_json(doc)), 200
 
 if __name__ == "__main__":
-    app.run(debug=True, host="127.0.0.1", port=5000)
+    app.run(debug=False, host="0.0.0.0", port=80)
